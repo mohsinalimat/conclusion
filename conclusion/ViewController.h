2b0e9d6a790351922776f1fcd699dc8ad1bf3ead
Laura <mac@macdeMacBook-Pro-2.local>
2014-11-29 18:11:11 +0800

    Initial Commit

---
+++ conclusion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  conclusion
+//
+//  Created by Laura on 14/11/29.
+//  Copyright (c) 2014年 Laura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ conclusion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  conclusion
+//
+//  Created by Laura on 14/11/29.
+//  Copyright (c) 2014年 Laura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
